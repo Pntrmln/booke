270b5a2ccdbdb3a7fd5962546e82fdb5d82bff10
tamadoo <zsamovankeszlethianyig@gmail.com>
2026-02-25 18:48:14 +0100

    minimális változtatások (áttérés jQuery-re)

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 function asd() {
     window.alert("szia faszos");
+    // $("#fejlec").addClass("disabled");
 }
-const fooldal = document.getElementById("food");
-const szolg = document.getElementById("szolg");
-const szob = document.getElementById("szob");
-const aszf = document.getElementById("aszf");
 function navbar(n) {
     if (n == "fooldal") {
-        fooldal.className = "p-2 nav-link disabled";
-        szolg.className = "p-2 nav-link";
-        szob.className = "p-2 nav-link";
-        aszf.className = "p-2 nav-link";
+        $("#foold").addClass("disabled");
+        $("#szolg").removeClass("disabled");
+        $("#szob").removeClass("disabled");
+        $("#aszf").removeClass("disabled");
     }
-    else if (n = "szolg") {
-        szolg.className = "p-2 nav-link";
-        fooldal.className = "p-2 nav-link";
-        szob.className = "p-2 nav-link disabled";
-        aszf.className = "p-2 nav-link disabled";
+    else if (n == "szolg") {
+        $("#szolg").removeClass("disabled");
+        $("#foold").removeClass("disabled");
+        $("#szob").addClass("disabled");
+        $("#aszf").addClass("disabled");
     }
-    else if (n = "szob") {
-        szob.className = "p-2 nav-link";
-        fooldal.className = "p-2 nav-link";
-        szolg.className = "p-2 nav-link disabled";
-        aszf.className = "p-2 nav-link disabled";
+    else if (n == "szob") {
+        $("#szob").removeClass("disabled");
+        $("#foold").removeClass("disabled");
+        $("#szolg").addClass("disabled");
+        $("#aszf").addClass("disabled");
     }
-    else if (n = "aszf") {
-        aszf.className = "p-2 nav-link";
-        fooldal.className = "p-2 nav-link";
-        szolg.className = "p-2 nav-link disabled";
-        szob.className = "p-2 nav-link disabled";
+    else if (n == "aszf") {
+        $("#aszf").removeClass("disabled");
+        $("#foold").removeClass("disabled");
+        $("#szolg").addClass("disabled");
+        $("#szob").addClass("disabled");
     }
 }
