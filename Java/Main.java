a85775b6b73d11dde9246cfbd7f301c4a665a7f8
P0gacsa <129489620+P0gacsa@users.noreply.github.com>
2026-03-10 12:23:51 +0100

    3.2.1

--- Java/Main.java
+++ Java/Main.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import javax.swing.*;
 
-class asd extends JFrame {
+class gui extends JFrame {
 
     static JFrame frame;
     static JComboBox<String> c;
@@ -22,13 +22,18 @@ class asd extends JFrame {
 
         frame.setSize(1280, 720);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setResizable(false);
+        frame.setResizable(true);
         frame.setLocationRelativeTo(null);
 
         GridBagLayout gb = new GridBagLayout();
         GridBagConstraints gbc = new GridBagConstraints();
 
-        frame.setLayout(gb);
+        BorderLayout bl = new BorderLayout();
+
+        JPanel bevitelMezoPanel = new JPanel(gb);
+        JPanel bevitelGombPanel = new JPanel();
+
+        frame.setLayout(bl);
 
         gbc.weightx = 0.5;
         gbc.gridy = 0;
@@ -37,29 +42,44 @@ class asd extends JFrame {
         int ivendegOszlopok = Integer.parseInt(strVendegOszlopok);
 
         ArrayList<TextArea> taLista = new ArrayList<>();
-
+        gbc.gridx = 0;
         for (int i = 0; i < ivendegOszlopok; i++){
-            gbc.gridx = i;
+            gbc.gridy = i;
             taLista.add(new TextArea(1, 25));
-            frame.add(taLista.get(i), gbc);
+            bevitelMezoPanel.add(taLista.get(i),gbc);
+            
         }
 
         c = new JComboBox<>();
-        gbc.gridx += 1;
-        frame.add(c, gbc);
+        gbc.gridy += 1;
+        bevitelMezoPanel.add(c, gbc);
+        
+        frame.add(bevitelMezoPanel, BorderLayout.PAGE_START);
 
         for (String arrSzobak1 : arrSzobak) {
             c.addItem(arrSzobak1);
         }
 
-        b = new JButton("asd");
-        gbc.gridx = 2;
-        gbc.gridy = 1;
-        frame.add(b, gbc);
+        b = new JButton("Foglalás rögzítése");
+        b.setBackground(Color.GREEN);
+        
+        bevitelGombPanel.add(b, gbc);
+        frame.add(bevitelGombPanel, BorderLayout.PAGE_END);
+
+        ArrayList<String> adatok = new ArrayList<>();
 
         b.addActionListener((ActionEvent e) -> {
-            System.out.println("asd");
+            for(int i = 0; i < taLista.size() ; i++) {
+                String adat = taLista.get(i).getText() + "";
+                adatok.add(adat);
+
+            }
+            adatok.add(c.getSelectedItem() + "");
+            System.out.println(adatok);
         });
+
+        
+
         frame.setVisible(true);
     }
 
@@ -92,6 +112,6 @@ class asd extends JFrame {
             System.out.println("Hiba: " + e.getMessage());
         }
 
-        new asd().bevitel();
+        new gui().bevitel();
     }
 }
