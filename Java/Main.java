500ede59cb6cd0f0843281ed6616a6eb0c42e2dd
P0gacsa <129489620+P0gacsa@users.noreply.github.com>
2026-03-17 12:25:52 +0100

    3.3.1

--- Java/Main.java
+++ Java/Main.java
@@ -1,11 +1,13 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.lang.reflect.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import javax.naming.spi.DirStateFactory;
 import javax.swing.*;
 
 class gui extends JFrame {
@@ -16,10 +18,15 @@ class gui extends JFrame {
     static String strVendegOszlopok;
     static int iSzobaOszlopok;
     static String[] arrSzobak;
+    static Connection conn;
+    static Statement st;
+    static ArrayList<String> vendegekMezo;
+    
 
     void bevitel() {
         frame = new JFrame();
 
+        frame.setTitle("bk_admin");
         frame.setSize(600, 800);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(true);
@@ -39,33 +46,35 @@ class gui extends JFrame {
 
         BevitelTab.setLayout(bl);
 
-        gbc.weightx = 0.5;
+        gbc.weightx = 0.2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
         int ivendegOszlopok = Integer.parseInt(strVendegOszlopok);
 
         ArrayList<TextArea> taLista = new ArrayList<>();
-        ArrayList<Component> comp = new ArrayList<>();
+        //ArrayList<Component> comp = new ArrayList<>();
         
-        for (int i = 0; i < ivendegOszlopok; i++){
+        for (int i = 0; i < ivendegOszlopok-1; i++){
             gbc.gridy = i;
             TextArea ta = new TextArea(1, 25);
             taLista.add(ta);
-            comp.add(ta);
+            //comp.add(ta);
             bevitelMezoPanel.add(ta,gbc);
         }
 
+        //JDateChooser asd = new JDateChooser();
+
         c = new JComboBox<>();
         c.addItem("");
-        comp.add(c);
+        //comp.add(c);
 
         gbc.gridy += 1;
         bevitelMezoPanel.add(c, gbc);
 
-        for (int i = 0; i < comp.size(); i++) {
+        for (int i = 0; i < vendegekMezo.size(); i++) {
             gbc.gridx = 1;
             gbc.gridy = i;
-            bevitelMezoPanel.add(new Label(String.valueOf(i)), gbc);
+            bevitelMezoPanel.add(new Label(String.valueOf(vendegekMezo.get(i))), gbc);
         }
     
         BevitelTab.add(bevitelMezoPanel, BorderLayout.PAGE_START);
@@ -84,7 +93,6 @@ class gui extends JFrame {
         tabs.addTab("Bevitel", BevitelTab);
 
         JPanel foglalasok = new JPanel();
-        foglalasok.add(new Label("Még nincs itt semmi!"));
         tabs.addTab("Foglalások", foglalasok);
 
         frame.add(tabs);
@@ -114,9 +122,18 @@ class gui extends JFrame {
                     taLista.get(i).setText("");
                 }
                 c.setSelectedIndex(0);
-                System.out.println(adatok);
-            
             }
+
+            String foglalas = "";
+            for (int i = 0; i < adatok.size(); i++) {
+                foglalas += adatok.get(i) + " ";
+            }
+
+            Label foglalasAdat = new Label();
+            foglalasAdat.setFont(new Font("Arial",Font.PLAIN, 15));
+            foglalasAdat.setText(foglalas);
+            foglalasok.add(foglalasAdat);
+
             adatok.clear();
         });
 
@@ -124,26 +141,38 @@ class gui extends JFrame {
 
     public static void main(String[] args) throws SQLException {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://pg.tamado.org:5432/Hotel", "postgres", "hungaryen");
-            Statement st = conn.createStatement();
+            conn = DriverManager.getConnection("jdbc:postgresql://pg.tamado.org:5432/Hotel", "postgres", "hungaryen");
+            st = conn.createStatement();
             ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM information_schema.columns WHERE table_name = 'vendegek'");
 
             while (rs.next()) {
                 strVendegOszlopok = rs.getString(1);
             }
 
-            ResultSet rs2 = st.executeQuery("SELECT COUNT(*) FROM szobak");
+            rs = st.executeQuery("SELECT COUNT(*) FROM szobak");
 
-            while (rs2.next()) {
-                iSzobaOszlopok = Integer.parseInt(rs2.getString(1));
+            while (rs.next()) {
+                iSzobaOszlopok = Integer.parseInt(rs.getString(1));
             }
             arrSzobak = new String[iSzobaOszlopok];
 
-            ResultSet rs3 = st.executeQuery("SELECT nev FROM szobak");
+            rs = st.executeQuery("SELECT nev FROM szobak");
 
-            for (int i = 0; i < iSzobaOszlopok && rs3.next(); i++){
-                arrSzobak[i] = rs3.getString(1);
+            for (int i = 0; i < iSzobaOszlopok && rs.next(); i++){
+                arrSzobak[i] = rs.getString(1);
             }
+
+            rs = st.executeQuery("SELECT table_name, column_name FROM information_schema.columns where table_name = 'vendegek'");
+            rs.next();
+
+            vendegekMezo = new ArrayList<>();
+            while (rs.next()) {
+                vendegekMezo.add(rs.getString(1)+"."+rs.getString(2));
+            }
+            rs = st.executeQuery("SELECT table_name, column_name FROM information_schema.columns where table_name = 'szobak'");
+            rs.next();
+            rs.next();
+            vendegekMezo.add(rs.getString(1)+"."+rs.getString(2));
 
         } catch (SQLException e) {
             System.out.println("Hiba: " + e.getMessage());
